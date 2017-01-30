777781f655a08e63a49237778ba499831d2a2597
konra <konra@DESKTOP-7OBCH2A>
2017-01-30 10:27:49 +0100

    Build fixes

--- Arxius.Tests/Program.cs
+++ Arxius.Tests/Program.cs
@@ -10,55 +10,55 @@ namespace Arxius.Tests
 
         static void Main(string[] args)
         {
-            MainAsync();
+            //MainAsync();
             Console.Read();
         }
 
-        static async void MainAsync()
-        {
-            bool testing = false;
-            var cService = new CoursesService();
-            var uService = new UtilsService();
-            #region Tests
-            if (testing)
-            {
-                var tests = new Tests();
-                Console.WriteLine("Authentication: " + await tests.IsAuthenticatedSuccessfully());
-                Stopwatch s = new Stopwatch();
-                s.Start();
-                Console.WriteLine("Current semester courses number correct (2): " + await tests.IsCorrectNumberOfCoursesInCurrentSemesterReturned());
-                Console.WriteLine("All courses number correct (29): " + await tests.IsCorrectNumberOfAllTimeCourses());
-                Console.WriteLine("Newsfeed download succesfull: " + await tests.IsNewsfeedDownloadSuccesfull());
-                Console.WriteLine("UserPage download succesfull: " + await tests.IsUserPageDownloadSuccesfull());
-                Console.WriteLine("Employelist counts 142 " + await tests.IsEmployeeListDownloaded());
-                s.Stop();
-                Console.WriteLine(s.Elapsed);
-                s = new Stopwatch();
-                s.Start();
-                Console.WriteLine("Current semester courses number correct (2): " + await tests.IsCorrectNumberOfCoursesInCurrentSemesterReturned());
-                Console.WriteLine("All courses number correct (29): " + await tests.IsCorrectNumberOfAllTimeCourses());
-                Console.WriteLine("Newsfeed download succesfull: " + await tests.IsNewsfeedDownloadSuccesfull());
-                Console.WriteLine("UserPage download succesfull: " + await tests.IsUserPageDownloadSuccesfull());
-                Console.WriteLine("Employelist counts 142 " + await tests.IsEmployeeListDownloaded());
-                s.Stop();
-                Console.WriteLine(s.Elapsed);
-                s = new Stopwatch();
-                s.Start();
-                Console.WriteLine("Current semester courses number correct (2): " + await tests.IsCorrectNumberOfCoursesInCurrentSemesterReturned());
-                Console.WriteLine("All courses number correct (29): " + await tests.IsCorrectNumberOfAllTimeCourses());
-                Console.WriteLine("Newsfeed download succesfull: " + await tests.IsNewsfeedDownloadSuccesfull());
-                Console.WriteLine("UserPage download succesfull: " + await tests.IsUserPageDownloadSuccesfull());
-                Console.WriteLine("Employelist counts 142 " + await tests.IsEmployeeListDownloaded());
-                s.Stop();
-                Console.WriteLine(s.Elapsed);
-            }
-            #endregion
-            else
-            {
-                await AuthDoNotSync.Login();
-               uService.GetImportantDates();
-            }
-            Console.Read();
-        }
+        //static async void MainAsync()
+        //{
+        //    bool testing = false;
+        //    var cService = new CoursesService();
+        //    var uService = new UtilsService();
+        //    #region Tests
+        //    if (testing)
+        //    {
+        //        var tests = new Tests();
+        //        Console.WriteLine("Authentication: " + await tests.IsAuthenticatedSuccessfully());
+        //        Stopwatch s = new Stopwatch();
+        //        s.Start();
+        //        Console.WriteLine("Current semester courses number correct (2): " + await tests.IsCorrectNumberOfCoursesInCurrentSemesterReturned());
+        //        Console.WriteLine("All courses number correct (29): " + await tests.IsCorrectNumberOfAllTimeCourses());
+        //        Console.WriteLine("Newsfeed download succesfull: " + await tests.IsNewsfeedDownloadSuccesfull());
+        //        Console.WriteLine("UserPage download succesfull: " + await tests.IsUserPageDownloadSuccesfull());
+        //        Console.WriteLine("Employelist counts 142 " + await tests.IsEmployeeListDownloaded());
+        //        s.Stop();
+        //        Console.WriteLine(s.Elapsed);
+        //        s = new Stopwatch();
+        //        s.Start();
+        //        Console.WriteLine("Current semester courses number correct (2): " + await tests.IsCorrectNumberOfCoursesInCurrentSemesterReturned());
+        //        Console.WriteLine("All courses number correct (29): " + await tests.IsCorrectNumberOfAllTimeCourses());
+        //        Console.WriteLine("Newsfeed download succesfull: " + await tests.IsNewsfeedDownloadSuccesfull());
+        //        Console.WriteLine("UserPage download succesfull: " + await tests.IsUserPageDownloadSuccesfull());
+        //        Console.WriteLine("Employelist counts 142 " + await tests.IsEmployeeListDownloaded());
+        //        s.Stop();
+        //        Console.WriteLine(s.Elapsed);
+        //        s = new Stopwatch();
+        //        s.Start();
+        //        Console.WriteLine("Current semester courses number correct (2): " + await tests.IsCorrectNumberOfCoursesInCurrentSemesterReturned());
+        //        Console.WriteLine("All courses number correct (29): " + await tests.IsCorrectNumberOfAllTimeCourses());
+        //        Console.WriteLine("Newsfeed download succesfull: " + await tests.IsNewsfeedDownloadSuccesfull());
+        //        Console.WriteLine("UserPage download succesfull: " + await tests.IsUserPageDownloadSuccesfull());
+        //        Console.WriteLine("Employelist counts 142 " + await tests.IsEmployeeListDownloaded());
+        //        s.Stop();
+        //        Console.WriteLine(s.Elapsed);
+        //    }
+        //    #endregion
+        //    else
+        //    {
+        //        await AuthDoNotSync.Login();
+        //        uService.GetImportantDates();
+        //    }
+        //    Console.Read();
+        //}
     }
 }
